d0aaa2222664528c32cf9fcf831a257f8fb72d3d
Mees-H <meesconfirmed@gmail.com>
2023-02-28 19:53:35 +0100

    meer velden aan form toegevoegd

--- specialGolfHaverlij/resources/views/trainingCMS.blade.php
+++ specialGolfHaverlij/resources/views/trainingCMS.blade.php
@@ -3,8 +3,29 @@
 @section('content')
 <form>
   <div class="form-group">
-    <label for="">Titel</label>
-    <input type="text" name="title" class="form-control" placeholder="Waarover gaat de training?">
+    <label for="name">Naam</label>
+    <input type="text" name="name" class="form-control" placeholder="Hoe wil je de training noemen?" required>
   </div>
+  <div class="form-group">
+    <label for="date">Datum</label>
+    <input type="date" name="date" class="form-control" placeholder="Op welke datum is de training?" required>
+  </div>
+  <div class="form-group">
+    <label for="start">Starttijd</label>
+    <input type="time" name="start" class="form-control" placeholder="Hoelaat begint de training?" required>
+  </div>
+  <div class="form-group">
+    <label for="end">Eindtijd</label>
+    <input type="time" name="end" class="form-control" placeholder="Hoelaat eindigt de training?" required>
+  </div>
+  <div class="form-group">
+    <label for="location">Locatie</label>
+    <input type="text" name="location" class="form-control" placeholder="Waar vindt de training plaats?" required>
+  </div>
+  <div class="form-group">
+    <label for="trainers">Trainers</label>
+    <input type="text" name="trainers" class="form-control" placeholder="Welke trainers geven deze training?" required>
+  </div>
+  <button type="submit" class="btn btn-gradient=primary me-2">Training toevoegen</button>
 </form>
 @stop
