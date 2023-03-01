118be975139d1a6f030229b92b8ff7962ecdc357
Mees-H <meesconfirmed@gmail.com>
2023-03-01 11:21:42 +0100

    removed comments

--- specialGolfHaverlij/public/js/app.js
+++ specialGolfHaverlij/public/js/app.js
@@ -1,8 +1,5 @@
 function getRowNumber(button) {
     var trainingen = document.getElementsByClassName("deleteTraining");
-    //for loop through the list of buttons
-    //if statement that checks if the button is the same as the button in the list
-    //return the index + 1
     for (let i = 0; i < trainingen.length; i++) {
         if (trainingen[i] == button) {
             return i;
@@ -17,7 +14,6 @@ function removeRow(button) {
         url: "../php/removeRow.php" ,
         data: { data: getRowNumber(button) },
         success : function() { 
-            // function below reloads current page
             location.reload();
         }
     });
