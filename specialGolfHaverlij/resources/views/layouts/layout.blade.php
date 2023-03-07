75a3d611b708e0fbeb0bf340ea493e4bd7650968
Mees-H <meesconfirmed@gmail.com>
2023-03-07 17:10:56 +0100

    made search bar

--- specialGolfHaverlij/resources/views/layouts/layout.blade.php
+++ specialGolfHaverlij/resources/views/layouts/layout.blade.php
@@ -40,6 +40,10 @@
                 <a class="nav-link" href="/team">Team</a>
             </li>
             </ul>
+                <form class="form-inline my-2 my-lg-0">
+                <input class="form-control mr-sm-2" type="search" placeholder="Zoek hier..." aria-label="Search">
+                <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Zoeken</button>
+            </form>
         </div>
         </nav>
         <div class="container">
