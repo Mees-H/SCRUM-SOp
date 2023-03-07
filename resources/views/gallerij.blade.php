d51a6639fc6d6766c16f7c0dafda910f660e1023
PerryWdm <ph.wildeman@student.avans.nl>
2023-03-07 20:38:37 +0100

    adding form

---
+++ resources/views/gallerij.blade.php
@@ -0,0 +1,15 @@
+@extends('layouts.layout')
+
+@section('content')
+
+    <div class="container justify-content-center text-center">
+        <h1>Gallerij</h1>
+        <p>Album</p>
+        <p>Album</p>
+        <p>Album</p>
+        <p>Album</p>
+        <p>Album</p>
+
+    <button class="btn btn-secondary" type="button" onclick="window.location='{{url("gallerij/aanmaken")}}'">Maak album aan</button>
+    </div>
+@stop
