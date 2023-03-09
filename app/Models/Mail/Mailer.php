9bc511b5d4dc12419bae150121736c851c387dec
KoenVerstappen <k.verstappen1@student.avans.nl>
2023-03-09 17:49:09 +0100

    makkelijker gemaakt om een mail naar een standaard te defini"eren (in .env) mail te sturen.

--- app/Models/Mail/Mailer.php
+++ app/Models/Mail/Mailer.php
@@ -5,20 +5,26 @@ namespace App\Models\Mail;
 use http\Exception\InvalidArgumentException;
 use Illuminate\Mail\Mailable;
 use Illuminate\Support\Facades\Mail;
+use League\Flysystem\Config;
 
 class Mailer
 {
     /**
-     * @param array $recepients an array of the recipients. the first element is assumed to be the primary recipient and the rest are bcc'd.
+     * @param array $recepients an array of the recipients. If $toStandard is false the first element is assumed to be the primary recipient and the rest are bcc'd.
+     * Otherwise the standard send to adress is used as recepient, and all the adresses in this array are BCC'd.
      * @param Mailable $mailable an object that extends laravel's Mailable class which represents the objact that is to be mailed.
+     * @param boolean $toStandard determines if a mail is to be sent to the standard email adress.
      * @return void
      */
-    public static function Mail($recepients, Mailable $mailable){
+    public static function Mail($recepients, Mailable $mailable, boolean $toStandard){
         if(count($recepients)< 1){
             throw new InvalidArgumentException();
         }
+        if($toStandard){
+            array_unshift($recepients,Config::get('mail.to.adress'));
+        }
 
-        $recepient = $recepients[0];
+        $recipient = $recepients[0];
 
         $bcc = [];
 
@@ -26,7 +32,7 @@ class Mailer
             array_push($bcc, $recepients[$i]);
         }
 
-        Mail::to($recepient)
+        Mail::to($recipient)
             ->bcc($bcc)
             ->send($mailable);
     }
