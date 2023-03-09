78c0494674b31aabdfaf49fbc7fb6fe58c2436a1
RikvanM <r.vanmourik1@student.avans.nl>
2023-03-09 20:52:58 +0100

    begin van footer aanmaken

--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -5,6 +5,8 @@
         <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
         <script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.3/dist/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
+        <link rel="stylesheet" href="/public/app.css">
+       <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.3.0/css/all.min.css" integrity="sha512-SzlrxWUlpfuzQ+pcUCosxcglQRNAq/DZjVsC0lE40xsADsfeQoEypE+enwcOiGjk/bSuGGKHEyjSoQ1zVisanQ==" crossorigin="anonymous" referrerpolicy="no-referrer" /> 
     </head>
     <body>
         <nav class="navbar navbar-expand-lg navbar-light bg-light">
@@ -66,5 +68,20 @@
         <div class="container">
             @yield('content')
         </div>
+        <footer>
+          <div class="footer-content">
+          <p>Secretariaat Stichting Special Golf: specialgolfhaverleij@gmail.com</p>
+          <p>Rekeningnummer Rabobank: NL 38 RABO 0118102206 o.v.v. Special Golf</p>
+          <p>KvK nr. 88714543 - RSIN 864744328 - ANBI</p>
+
+          <ul class='socials'>
+             <li alt="link naar het facebook account"><a href=https://www.facebook.com/DeHaverleij/?locale=nl_NL ><i class="fa-brands fa-facebook" ></i></a></li>
+             <li  alt="link naar het instagram account"><a href=”https://www.instagram.com/specialgolf2021/”><i class="fa-brands fa-instagram"></i></a></li>
+             <li  alt="link naar het twitter account"><a href=”https://twitter.com/BGHaverleij”><i class="fa-brands fa-twitter"></i></a></li>
+             <li  alt="link naar het linkedin account"><a href=”#”><i class="fa-brands fa-linkedin"></i></a></li>
+          </ul>
+
+          </div>
+        </footer>
     </body>
 </html>
