9348f4346977f654d069800d2e63acc0d39ff20d
Phwdm <ph.wildeman@student.avans.nl>
2023-03-10 13:33:29 +0100

    showing albums

--- resources/views/galerij.blade.php
+++ resources/views/galerij.blade.php
@@ -3,13 +3,24 @@
 @section('content')
     <div class="container justify-content-center text-center">
         <h1>Gallerij</h1>
-        <div class="row justify-content-center">
+        <div class="row justify-content-center p-3">
              @foreach($albums as $album)
                  <div class="col-md-4">
-                     {{$album->id}}
-                 </div>
-                <div>
-                    {{$album->titel}}
+                     <div class="card">
+                        <div class="card-header">
+                            <h2>
+                            {{$album->title}}
+                            </h2>
+                        </div>
+                         <div class="card-body">
+                        {{--     <a href="{{url("galerij/$album->id")}}">--}}
+                                 {{--<img src="{{asset("storage/$album->coverImage")}}" class="img-fluid" alt="Afbeelding {{$album->title}}">--}}
+                            {{-- </a>--}}
+                         </div>
+                         <div class="card-footer">
+                             {{$album->date}}
+                        </div>
+                     </div>
                 </div>
              @endforeach
         </div>
