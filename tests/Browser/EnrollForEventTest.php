2fbe2db655159dc0f4c997d8b2114a8582e51ca6
JoshavEngelen <josha@mixnmac.nl>
2023-03-16 11:21:13 +0100

    chore Test: 1 test made for formsuccess

--- tests/Browser/EnrollForEventTest.php
+++ tests/Browser/EnrollForEventTest.php
@@ -11,11 +11,20 @@ class EnrollForEventTest extends DuskTestCase
     /**
      * A Dusk test example.
      */
-    public function testExample(): void
+    public function testFormSuccess(): void
     {
         $this->browse(function (Browser $browser) {
-            $browser->visit('/')
-                    ->assertSee('Laravel');
+            $browser->visit('/aanmelden')
+                    ->type('name', 'Test')
+                    ->type('birthday', '2000-01-01')
+                    ->type('email', 'test@gmail.com')
+                    ->type('phonenumber', '0612345678')
+                    ->type('address', 'Teststraat 1')
+                    ->type('city', 'Teststad')
+                    ->type('disability', 'Geen')
+                    ->type('event_id', '1')
+                    ->press('Aanmelden')
+                    ->assertSee('success');
         });
     }
 }
