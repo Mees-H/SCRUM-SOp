70bc53dd2694833467293e952bb5804437ed6061
KoenVerstappen <k.verstappen1@student.avans.nl>
2023-03-16 13:45:52 +0100

    tests voor eerste 4 invoervelden

--- tests/Browser/EnrollForEventTest.php
+++ tests/Browser/EnrollForEventTest.php
@@ -28,4 +28,74 @@ class EnrollForEventTest extends DuskTestCase
                     ->assertSee('Uw aanmelding is verzonden!');
         });
     }
+
+    public function testForm_BadName_shouldFail(): void
+    {
+        $this->browse(function (Browser $browser) {
+            $browser->visit('/aanmelden')
+                ->assertSee('Inschrijven voor Evenement')
+                ->type('name', 'Test4;drop table users;')
+                ->type('birthday', '2000-01-01')
+                ->type('email', 'test@gmail.com')
+                ->type('phonenumber', '0612345678')
+                ->type('address', 'Teststraat 1')
+                ->type('city', 'Teststad')
+                ->type('disability', 'Geen')
+                ->type('event_id', '1')
+                ->press('aanmeldknop')
+                ->assertSee('500');
+        });
+    }
+    public function testForm_BadBithday_shouldFail(): void
+    {
+        $this->browse(function (Browser $browser) {
+            $browser->visit('/aanmelden')
+                ->assertSee('Inschrijven voor Evenement')
+                ->type('name', 'Test')
+                ->type('birthday', 'ajnfdskjnjafnknsjdkfbg b')
+                ->type('email', 'test@gmail.com')
+                ->type('phonenumber', '0612345678')
+                ->type('address', 'Teststraat 1')
+                ->type('city', 'Teststad')
+                ->type('disability', 'Geen')
+                ->type('event_id', '1')
+                ->press('aanmeldknop')
+                ->assertSee('500');
+        });
+    }
+    public function testForm_BadEmail_shouldFail(): void
+    {
+        $this->browse(function (Browser $browser) {
+            $browser->visit('/aanmelden')
+                ->assertSee('Inschrijven voor Evenement')
+                ->type('name', 'Test')
+                ->type('birthday', '2000-01-01')
+                ->type('email', 'adsbfiobadsuygbfoulabfuygbdfvhybalxbvudsblyuierub')
+                ->type('phonenumber', '0612345678')
+                ->type('address', 'Teststraat 1')
+                ->type('city', 'Teststad')
+                ->type('disability', 'Geen')
+                ->type('event_id', '1')
+                ->press('aanmeldknop')
+                ->assertSee('Inschrijven voor Evenement');
+        });
+    }
+    public function testForm_BadPhoneNumber_shouldFail(): void
+    {
+        $this->browse(function (Browser $browser) {
+            $browser->visit('/aanmelden')
+                ->assertSee('Inschrijven voor Evenement')
+                ->type('name', 'Test')
+                ->type('birthday', '2000-01-01')
+                ->type('email', 'test@gmail.com')
+                ->type('phonenumber', '456as4df54fd65a4sdf65saf')
+                ->type('address', 'Teststraat 1')
+                ->type('city', 'Teststad')
+                ->type('disability', 'Geen')
+                ->type('event_id', '1')
+                ->press('aanmeldknop')
+                ->assertSee('500');
+        });
+    }
+
 }
