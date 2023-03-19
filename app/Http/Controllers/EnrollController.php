3b9668bf6890805458c349a2689e2f26573c4e03
JoshavEngelen <josha@mixnmac.nl>
2023-03-19 23:49:11 +0100

    revert previous commit

--- app/Http/Controllers/EnrollController.php
+++ app/Http/Controllers/EnrollController.php
@@ -6,7 +6,6 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Mail\Mailer;
 use App\Models\Mail\MailFactory;
-use App\Http\Requests\MailPostRequest;
 
 class EnrollController extends Controller
 {
