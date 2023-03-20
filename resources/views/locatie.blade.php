3c0f640fb42dc97fcb94a3fb62b9a195b9b9ad98
Mees-H <meesconfirmed@gmail.com>
2023-03-20 20:46:27 +0100

    fixed hamburgermenu and accessability

--- resources/views/locatie.blade.php
+++ resources/views/locatie.blade.php
@@ -1,12 +1,5 @@
 @extends('layouts.layout')
  
 @section('content')
-    <p>testtestest</p>
-    <p>testtestest</p>
-    <p>testtestest</p>
-    <p>testtestest</p>
-    <p>testtestest</p>
-    <p>testtestest</p>
-    <p>testtestest</p>
-    <p>testtestest</p>
+
 @stop
