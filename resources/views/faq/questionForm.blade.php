9a0f14b3bec620433e9c242a205a45063216e422
JoshavEngelen <josha@mixnmac.nl>
2023-03-25 14:34:19 +0100

    chore Validation: added required stars

--- resources/views/faq/questionForm.blade.php
+++ resources/views/faq/questionForm.blade.php
@@ -12,21 +12,21 @@
     <form method="POST" action="/faq/submit">
         @csrf
         <div class="form-group">
-            <div class="row"><label for="name">Voor- en achternaam</label></div>
+            <div class="row"><span class="requiredStar">*</span><label for="name">Voor- en achternaam</label></div>
             <input type="text" class="form-control" id="name" placeholder="bv: Jan de Graaf" name="name" value="{{old('name')}}">
             @error('name')
             <div class="alert alert-danger">{{ $message }}</div>
             @enderror
         </div>
         <div class="form-group">
-            <div class="row"><label for="email">Email adres</label></div>
+            <div class="row"><span class="requiredStar">*</span><label for="email">Email adres</label></div>
             <input type="email" class="form-control" id="email" placeholder="bv: jandegraaf@gmail.com" name="email"  value="{{old('email')}}">
             @error('email')
             <div class="alert alert-danger">{{ $message }}</div>
             @enderror
         </div>
         <div class="form-group">
-            <div class="row"><label for="question">Vraag</label></div>
+            <div class="row"><span class="requiredStar">*</span><label for="question">Vraag</label></div>
             <textarea type="text" class="form-control" id="question" placeholder="Vul hier uw vraag in" 
                 name="question">{{old('question')}}</textarea>
             @error('question')
