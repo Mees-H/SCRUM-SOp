f58c28b3ae07bd0dabed95e9d53095ab4b4a81f6
JoshavEngelen <josha@mixnmac.nl>
2023-03-26 20:48:56 +0200

    chore Form: slightly changed layout of the form

--- resources/views/faq/questionForm.blade.php
+++ resources/views/faq/questionForm.blade.php
@@ -11,24 +11,37 @@
 
     <form method="POST" action="/faq/submit">
         @csrf
-        <div class="form-group">
-            <div class="row"><span class="requiredStar">*</span><label for="name">Voor- en achternaam</label></div>
-            <input type="text" class="form-control" id="name" placeholder="bv: Jan de Graaf" name="name" value="{{old('name')}}">
+        <div class="row mb-3">
+            <label for="name" class="col-sm-2 col-form-label">
+                <span class="requiredStar">*</span>Voor- en achternaam
+            </label>
+            <div class="col-sm-8">
+                <input type="text" class="form-control" id="name" placeholder="bv: Jan de Graaf" name="name" value="{{old('name')}}">
+            </div>
             @error('name')
             <div class="alert alert-danger">{{ $message }}</div>
             @enderror
         </div>
-        <div class="form-group">
-            <div class="row"><span class="requiredStar">*</span><label for="email">Email adres</label></div>
-            <input type="email" class="form-control" id="email" placeholder="bv: jandegraaf@gmail.com" name="email"  value="{{old('email')}}">
+        <div class="row mb-3">
+            <label for="email" class="col-sm-2 col-form-label">
+                <span class="requiredStar">*</span>Email adres
+            </label>
+            <div class="col-sm-8">
+                <input type="email" class="form-control" id="email" placeholder="bv: jandegraaf@gmail.com" name="email"  value="{{old('email')}}">
+                <div class="form-text">Het antwoord op uw vraag zal u ontvangen op dit e-mail adres.</div>
+            </div>
             @error('email')
             <div class="alert alert-danger">{{ $message }}</div>
             @enderror
         </div>
-        <div class="form-group">
-            <div class="row"><span class="requiredStar">*</span><label for="question">Vraag</label></div>
-            <textarea type="text" class="form-control" id="question" placeholder="Vul hier uw vraag in" 
-                name="question">{{old('question')}}</textarea>
+        <div class="row mb-3">
+            <label for="question" class="col-sm-2 col-form-label">
+                <span class="requiredStar">*</span>Vraag
+            </label>
+            <div class="col-sm-8">
+                <textarea type="text" class="form-control" id="question" placeholder="Vul hier uw vraag in" 
+                    name="question">{{old('question')}}</textarea>
+            </div>
             @error('question')
             <div class="alert alert-danger">{{ $message }}</div>
             @enderror
