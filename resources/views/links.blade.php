3055002dfb155f90eb155acd23bcf5adec5a596c
Qoen1 <k.verstappen1@student.avans.nl>
2023-03-27 14:06:53 +0200

    layout mooier gestyled

--- resources/views/links.blade.php
+++ resources/views/links.blade.php
@@ -4,16 +4,13 @@
     <div class="">
         <h1 class="text-center specialBrown p-4"><b>Links</b></h1>
     @foreach($links as $category)
-        <h2 class="specialBrown">{{$category['name']}}</h2>
-        <div class="container">
+            <div class="container"><div class="row">
+                <div class="col d-flex"><h2 class="specialBrown">{{$category['name']}}</h2></div>
+                    <div class="col">
         @foreach($category['links'] as $link)
-            <div class="row">
-                <div class="col">{{$link['name']}}</div>
-                <div class="col">{{$link['name']}}</div>
-                <div class="row col"><a class="siteMap effect-underline justify-content-between text-center
-            " href="{{$link['link']}}">{{$link['name']}}</a><div></div></div>
-            </div>
-        @endforeach
+                <div class="row"><a class="siteMap effect-underline" href="{{$link['link']}}">{{$link['name']}}</a><div></div></div>
+        @endforeach</div>
+                </div>
         </div>
         <hr>
     @endforeach
