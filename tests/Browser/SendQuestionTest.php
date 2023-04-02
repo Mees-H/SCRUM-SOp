45bfa31b93cbe87b6f57a18d32fce8c03982c7c8
JoshavEngelen <josha@mixnmac.nl>
2023-04-02 16:35:17 +0200

    chore Test: It seems that unit testing has already been covered for mails and a lot of tests were unnecessary due to the nature of validation

--- tests/Browser/SendQuestionTest.php
+++ tests/Browser/SendQuestionTest.php
@@ -22,82 +22,10 @@ class SendQuestionTest extends DuskTestCase
         });
     }
 
-    public function testForm_BadName_shouldFail(): void
+    public function testFormFailed(): void
     {
         $this->browse(function (Browser $browser) {
-            $string = '';
-            for ($i = 0; $i < 256; $i++) {
-                $string .= 'a';
-            }
-
-            //Test required field
-            $browser->visit('/faq/vraagformulier/')
-                ->assertSee('Stel een vraag')
-                ->type('name', '')
-                ->type('email', 'test@gmail.com')
-                ->type('question', 'Hoe werkt de evenementen pagina?')
-                ->press('verstuurknop')
-                ->waitForText('Uw naam is verplicht');
-
-            //Test max length
-            $browser->visit('/faq/vraagformulier/')
-                ->assertSee('Stel een vraag')
-                ->type('name', $string)
-                ->type('email', 'test@gmail.com')
-                ->type('question', 'Hoe werkt de evenementen pagina?')
-                ->press('verstuurknop')
-                ->waitForText('Uw naam mag niet langer zijn dan 255 karakters');
-        });
-    }
-
-    public function testForm_BadEmail_shouldFail(): void
-    {
-        $this->browse(function (Browser $browser) {
-            $string = '';
-            for ($i = 0; $i < 256; $i++) {
-                $string .= 'a';
-            }
-
-            $email = 'adsbfiobadsuygbfoulabfuygbdfvhybalxbvudsblyuierub';
-
-            //Test required field
-            $browser->visit('/faq/vraagformulier/')
-                ->assertSee('Stel een vraag')
-                ->type('name', 'Test')
-                ->type('email', '')
-                ->type('question', 'Hoe werkt de evenementen pagina?')
-                ->press('verstuurknop')
-                ->waitForText('Uw email is verplicht');
-
-            // //Test e-mail validation
-            // $browser->visit('/faq/vraagformulier/')
-            //     ->assertSee('Stel een vraag')
-            //     ->type('name', 'Test')
-            //     ->type('email', 'adsbfiobadsuygbfoulabfuygbdfvhybalxbvudsblyuierub')
-            //     ->type('question', 'Hoe werkt de evenementen pagina?')
-            //     ->press('verstuurknop')
-            //     ->
-
-            //Test max length
-            $browser->visit('/faq/vraagformulier/')
-                ->assertSee('Stel een vraag')
-                ->type('name', 'Test')
-                ->type('email', $string)
-                ->type('question', 'Hoe werkt de evenementen pagina?')
-                ->press('verstuurknop')
-                ->waitForText('Uw email mag niet langer zijn dan 255 karakters');
-        });
-    }
-
-    public function testForm_BadQuestion_shouldFail(): void
-    {
-        $this->browse(function (Browser $browser) {
-            $string = '';
-            for ($i = 0; $i < 501; $i++) {
-                $string .= 'a';
-            }
-
-            //Test required field
+            // Question validation
             $browser->visit('/faq/vraagformulier/')
                 ->assertSee('Stel een vraag')
                 ->type('name', 'Test')
@@ -105,15 +33,6 @@ class SendQuestionTest extends DuskTestCase
                 ->type('question', '')
                 ->press('verstuurknop')
                 ->waitForText('Het invoeren van de vraag is verplicht');
-
-            //Test max length
-            $browser->visit('/faq/vraagformulier/')
-                ->assertSee('Stel een vraag')
-                ->type('name', 'Test')
-                ->type('email', 'test@gmail.com')
-                ->type('question', $string)
-                ->press('verstuurknop')
-                ->waitForText('Uw vraag mag niet langer zijn dan 500 karakters');
         });
     }
 }
