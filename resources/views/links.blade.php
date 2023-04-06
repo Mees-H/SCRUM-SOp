22ed21bd948208c4c281cbf0f6b2ce950a6724fe
KoenVerstappen <k.verstappen1@student.avans.nl>
2023-04-06 13:46:20 +0200

    andere styling

--- resources/views/links.blade.php
+++ resources/views/links.blade.php
@@ -3,16 +3,23 @@
 @section('content')
     <div class="">
         <h1 class="text-center specialBrown p-4"><b>Links</b></h1>
-    @foreach($links as $category)
-            <div class="container"><div class="row">
-                <div class="col d-flex"><h2 class="specialBrown">{{$category['name']}}</h2></div>
-                    <div class="col">
-        @foreach($category['links'] as $link)
-                <div class="row"><a class="siteMap" href="{{$link['link']}}">{{$link['name']}}</a><div></div></div>
-        @endforeach</div>
+        <div class="d-flex flex-row flex-wrap justify-content-center">
+            @foreach($links as $category)
+                <div class="w-25 pb-3">
+                    <div class="">
+                        <div class="">
+                            <h2 class="specialBrown">{{$category['name']}}</h2>
+                        </div>
+                        <div class="col">
+                            @foreach($category['links'] as $link)
+                                <div class="row">
+                                    <a href="{{$link['link']}}">{{$link['name']}}</a>
+                                </div>
+                            @endforeach
+                        </div>
+                    </div>
                 </div>
+            @endforeach
         </div>
-        <hr>
-    @endforeach
     </div>
 @stop
