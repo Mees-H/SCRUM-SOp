1deb298aa324e49f9e5814e135ceca156fbeee86
KoenVerstappen <k.verstappen1@student.avans.nl>
2023-04-06 15:13:25 +0200

    tests toegevoegd die ckecken of alle links in de JSON en de jaren er zijn.

--- tests/Browser/SitemapTest.php
+++ tests/Browser/SitemapTest.php
@@ -2,7 +2,9 @@
 
 namespace Tests\Browser;
 
+use App\Models\Album;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Support\Facades\Storage;
 use Laravel\Dusk\Browser;
 use Tests\DuskTestCase;
 
@@ -17,6 +19,35 @@ class SitemapTest extends DuskTestCase
             $browser->visit('/links')
                 ->clickLink("team")
                 ->assertPathIs("/team");
+        });
+    }
+
+
+    public function testAllLinks() : void
+    {
+        $this->browse(function (Browser $browser) {
+            $links = json_decode(Storage::disk('public')->get('sitemap.json'),true);
+            $links = array_filter($links['categories'], function ($category) {
+                return $category['private'] !== true;
+            });
+            $years = Album::all()->pluck('date')->map(function ($date) {
+                return (new \DateTime($date))->format('Y');
+            })->unique()->toArray();
+
+            foreach ($links as $category){
+                foreach ($category['links'] as $link){
+                    $browser->visit('/links')
+                        ->assertSee($category['name'])
+                        ->clickLink($link['name'])
+                        ->assertPathIs($link['link']);
+                }
+            }
+
+            foreach ($years as $year){
+                $browser->visit('/links')
+                    ->clickLink($year)
+                    ->assertPathIs("/galerij/$year");
+            }
         });
     }
 }
