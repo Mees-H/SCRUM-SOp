f9bd91324f0e09ad5064e570d0be0a7414f2235f
JoshavEngelen <josha@mixnmac.nl>
2023-04-07 16:06:07 +0200

    chore Userlist: Now has a list of users for the admin

---
+++ resources/views/admin/userlist.blade.php
@@ -0,0 +1,42 @@
+@extends('layouts.layout')
+
+@section('content')
+
+<div class="py-12">
+    <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+        <div class="p-6 text-gray-900">
+            <a href="/admin/create" class="btn btn-primary m-1">{{__('Voeg gebruiker toe')}}</a>
+        </div>
+    </div>
+</div>
+
+<br />
+
+<div class="py-12">
+    <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-6">
+        <div class="p-4 sm:p-8 bg-white dark:bg-gray-800 shadow sm:rounded-lg">
+            <div class="max-w-xl">
+                <table class="table">
+                    <thead>
+                    <th>{{__('Gebruikersnaam')}}</th>
+                    <th>{{__('Email')}}</th>
+                    <th>{{__('Functie')}}</th>
+                    <th>{{__('Verwijder gebruiker')}}</th>
+                    <th></th>
+                    </thead>
+
+                    @foreach($users as $user)
+                        <tr>
+                            <td>{{$user->name}}</td>
+                            <td>{{$user->email}}</td>
+                            <td>{{$user->role}}</td>
+                            <td></td>
+                        </tr>
+                    @endforeach
+                </table>
+            </div>
+        </div>
+    </div>
+</div>
+
+@stop
