61aaf8ee42fd22ab177413360df84d402ac231b2
JoshavEngelen <josha@mixnmac.nl>
2023-04-07 15:39:42 +0200

    chore User: Can create coaches and supervisors as admin

---
+++ app/Http/Controllers/Admin/CreateUserController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use App\Providers\RouteServiceProvider;
+use Illuminate\Auth\Events\Registered;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\Rules;
+
+class CreateUserController extends Controller
+{
+    public function adminIndex()
+    {
+        // TO-DO: Adjust to User list
+        return view('admin.createUser');
+    }
+
+    public function storeUser(Request $request)
+    {
+        $request->validate([
+            'name' => ['required', 'string', 'max:255'],
+            'email' => ['required', 'string', 'email', 'max:255', 'unique:'.User::class],
+            'password' => ['required', 'confirmed', Rules\Password::defaults()],
+            'password_confirmation' => 'required'
+        ]);
+
+        //ddd($request);
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+            'role' => $request->role
+        ]);
+
+        event(new Registered($user));
+
+        return redirect('/admin/create')->with('success', 'Gebruiker aangemaakt!');
+    }
+}
