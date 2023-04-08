89f0f9c61a6183176f476b703181faa87be6c0ee
Mees-H <meesconfirmed@gmail.com>
2023-04-08 17:09:48 +0200

    moved login and search bar

--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -1,5 +1,3 @@
-@if (Auth::user() == null)
-<!-- Layout als je niet ingelogd bent -->
 <!DOCTYPE html>
 <html lang="nl">
     <head>
@@ -21,8 +19,10 @@
 
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.3.0/css/all.min.css" integrity="sha512-SzlrxWUlpfuzQ+pcUCosxcglQRNAq/DZjVsC0lE40xsADsfeQoEypE+enwcOiGjk/bSuGGKHEyjSoQ1zVisanQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
         <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-
     </head>
+
+@if (Auth::user() == null)
+<!-- Layout als je niet ingelogd bent -->
     <body>
         <nav class="navbar navbar-expand-xl navbar-light bg-light">
         <a class="navbar-brand text-dark" href="/"><img src="/img/specialgolflogodark.png" aria-label="Logo van Special Golf Haverlij, een kleurrijke zwaan" id="logo"></a>
@@ -31,7 +31,7 @@
         </button>
 
         <div class="collapse navbar-collapse" id="main-navbar">
-            <ul class="navbar-nav mr-auto">
+            <ul class="navbar-nav me-auto">
             <li class="nav-item">
                 <a class="nav-link text-dark {{ (request()->segment(1) == 'index') ? 'font-weight-bold' : '' }}" href="/">Startpagina</a>
             </li>
@@ -80,103 +80,61 @@
             <li class="nav-item">
                 <a class="nav-link text-dark {{ (request()->segment(1) == 'links') ? 'font-weight-bold' : '' }}" href="/links">Links</a>
             </li>
-            <li class="nav-item">
-                <a class="nav-link text-dark {{ (request()->segment(1) == 'login') ? 'font-weight-bold' : '' }}" href="/login">Login</a>
-            </li>
             </ul>
-            <form class="form-inline my-2 my-lg-0 position-relative" method="GET" action="#" >
-                <input class="form-control mr-sm-2 search"id="dropdown" type="search" name="search" placeholder="Zoek hier..." aria-label="Search" onkeyup="FilterWords()">
-                <ul class="border border-dark rounded d-none" id="content">
-                    <li><a class="dropdown-item text-center" href="/">
-                        Hoofdpagina
-                    </a></li>
-                    <li><a class="dropdown-item text-center" href="/training">
-                        Trainingen
-                    </a></li>
-                    <li><a class="dropdown-item text-center" href="/evenement">
-                        Evenementen
-                    </a></li>
-                    <li><a class="dropdown-item text-center" href="/galerij/2023">
-                        2023
-                    </a></li>
-                    <li><a class="dropdown-item text-center" href="/galerij/2022">
-                        2022
-                    </a></li>
-                    <li> <a class="dropdown-item text-center" href="/galerij/2021">
-                        2021
-                    </a></li>
-                    <li><a class="dropdown-item text-center" href="/faq">
-                        FAQ
-                    </a></li>
-                    <li><a class="dropdown-item text-center" href="/nieuwsbrief">
-                        Nieuwsbrief
-                    </a></li>
-                    <li><a class="dropdown-item text-center" href="/team">
-                        Team
-                    </a></li>
-                    <li><a class="dropdown-item text-center" href="/partner">
-                        Partner
-                    </a></li>
-                    <li><a class="dropdown-item text-center" href="/overons">
-                        Over Ons
-                    </a></li>
-                    <li><a class="dropdown-item text-center" href="/locatie">
-                        Locatie
-                    </a></li>
-                    <li><a class="dropdown-item text-center" href="/links">
-                        Links
-                    </a></li>
-                </ul>
-            </form>
+            <ul class="navbar-nav ms-auto">
+                <li class="nav-item">
+                    <a class="nav-link text-dark {{ (request()->segment(1) == 'login') ? 'font-weight-bold' : '' }}" href="/login">Login</a>
+                </li>
+                <form class="form-inline my-2 my-lg-0 position-relative" method="GET" action="#" >
+                    <input class="form-control mr-sm-2 search"id="dropdown" type="search" name="search" placeholder="Zoek hier..." aria-label="Search" onkeyup="FilterWords()">
+                    <ul class="border border-dark rounded d-none" id="content">
+                        <li><a class="dropdown-item text-center" href="/">
+                            Hoofdpagina
+                        </a></li>
+                        <li><a class="dropdown-item text-center" href="/training">
+                            Trainingen
+                        </a></li>
+                        <li><a class="dropdown-item text-center" href="/evenement">
+                            Evenementen
+                        </a></li>
+                        <li><a class="dropdown-item text-center" href="/galerij/2023">
+                            2023
+                        </a></li>
+                        <li><a class="dropdown-item text-center" href="/galerij/2022">
+                            2022
+                        </a></li>
+                        <li> <a class="dropdown-item text-center" href="/galerij/2021">
+                            2021
+                        </a></li>
+                        <li><a class="dropdown-item text-center" href="/faq">
+                            FAQ
+                        </a></li>
+                        <li><a class="dropdown-item text-center" href="/nieuwsbrief">
+                            Nieuwsbrief
+                        </a></li>
+                        <li><a class="dropdown-item text-center" href="/team">
+                            Team
+                        </a></li>
+                        <li><a class="dropdown-item text-center" href="/partner">
+                            Partner
+                        </a></li>
+                        <li><a class="dropdown-item text-center" href="/overons">
+                            Over Ons
+                        </a></li>
+                        <li><a class="dropdown-item text-center" href="/locatie">
+                            Locatie
+                        </a></li>
+                        <li><a class="dropdown-item text-center" href="/links">
+                            Links
+                        </a></li>
+                    </ul>
+                </form>
+            </ul>
         </div>
         </nav>
-
-        <div class="container">
-            @yield('content')
-        </div>
-        <footer>
-          <div class="footer-content bg-light">
-           <div class="footer-info">
-          <p class="text-dark">Secretariaat Stichting Special Golf: specialgolfhaverleij@gmail.com</p>
-          <p class="text-dark">Rekeningnummer Rabobank: NL 38 RABO 0118102206 o.v.v. Special Golf</p>
-          <p class="text-dark">KvK nr. 88714543 - RSIN 864744328 - ANBI</p>
-          </div>
-          <ul class='socials text-dark'>
-             <li><a class="text-dark" aria-label="Icoontje met link naar het Facebook account van Special Golf Haverlij" href=https://www.facebook.com/DeHaverleij/?locale=nl_NL  ><i class="fa-brands fa-facebook" ></i></a></li>
-             <li><a class="text-dark" aria-label="Icoontje met link naar het Instagram account van Special Golf Haverlij" href=https://www.instagram.com/specialgolf2021/  ><i class="fa-brands fa-instagram"></i></a></li>
-          </ul>
-
-          </div>
-        </footer>
-    </body>
-</html>
-
 @else
 
 <!-- Layout als je wel ingelogd bent-->
-<!DOCTYPE html>
-<html lang="nl">
-    <head>
-        <title>Special Golf Haverlij</title>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.3.0/font/bootstrap-icons.css">
-        <meta name="csrf-token" content="{{ csrf_token() }}">
-        
-        <!-- boostrap required voor slider -->
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous" defer></script>
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
-
-
-        <link rel="stylesheet" href="{{ asset('/css/slider.css') }} ">
-        <link rel="stylesheet" href="{{ asset('/css/dropdown.css') }} ">
-        <link rel="stylesheet" href="{{ asset('css/app.css')}}">
-        <script src="{{ asset('/js/dropdown.js') }}" defer></script>
-
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.3.0/css/all.min.css" integrity="sha512-SzlrxWUlpfuzQ+pcUCosxcglQRNAq/DZjVsC0lE40xsADsfeQoEypE+enwcOiGjk/bSuGGKHEyjSoQ1zVisanQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-        <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-
-    </head>
     <body>
         <nav class="navbar navbar-expand-xl navbar-light bg-light">
         <a class="navbar-brand text-dark" href="/"><img src="/img/specialgolflogodark.png" aria-label="Logo van Special Golf Haverlij, een kleurrijke zwaan" id="logo"></a>
@@ -185,7 +143,7 @@
         </button>
 
         <div class="collapse navbar-collapse" id="main-navbar">
-            <ul class="navbar-nav mr-auto">
+            <ul class="navbar-nav me-auto">
             <li class="nav-item">
                 <a class="nav-link text-dark {{ (request()->segment(1) == 'index') ? 'font-weight-bold' : '' }}" href="/">Startpagina</a>
             </li>
@@ -222,7 +180,9 @@
             <li class="nav-item">
                 <a class="nav-link text-dark {{ (request()->segment(1) == 'slider') ? 'font-weight-bold' : '' }}" href="/slider">Slider</a>
             </li>
-            <li class="nav-item dropdown">
+            </ul>
+            <ul class="navbar-nav ms-auto">
+                <li class="nav-item dropdown">
                     <a class="nav-link dropdown-toggle text-dark {{ (request()->segment(1) == 'profile') ? 'font-weight-bold' : '' }}" id="navbarDropdown" role="button" data-bs-toggle="dropdown"  aria-haspopup="true" aria-expanded="false">
                         {{ Auth::user()->name }}
                     </a>
@@ -243,8 +203,7 @@
                         </li>
                     </ul>
                 </li>
-            </ul>
-            <form class="form-inline my-2 my-lg-0 position-relative" method="GET" action="#" >
+                <form class="form-inline my-2 my-lg-0 position-relative" method="GET" action="#" >
                 <input class="form-control mr-sm-2 search"id="dropdown" type="search" name="search" placeholder="Zoek hier..." aria-label="Search" onkeyup="FilterWords()">
                 <ul class="border border-dark rounded d-none" id="content">
                     <li><a class="dropdown-item text-center" href="/index">
@@ -288,10 +247,12 @@
                     </a></li>
                 </ul>
             </form>
+            </ul>
         </div>
         </nav>
+@endif
 
-        <div class="container">
+<div class="container">
             @yield('content')
         </div>
         <footer>
@@ -310,4 +271,3 @@
         </footer>
     </body>
 </html>
-@endif
