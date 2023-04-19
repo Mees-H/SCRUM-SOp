60799bdc065c59dc6e3679479cd5fd5afa99a061
Phwdm <ph.wildeman@student.avans.nl>
2023-04-19 14:57:10 +0200

    Adding sidebar to newspage

---
+++ resources/views/components/SideBar/SideBarNavigation.blade.php
@@ -0,0 +1,23 @@
+
+<!-- Sidebar -->
+<nav id="sidebar">
+    <div class="sidebar-header">
+        <h3>Bootstrap Sidebar</h3>
+    </div>
+
+    <ul class="list-unstyled components">
+        <p>Dummy Heading</p>
+        <li class="active">
+            <a href="#">Home 1</a>
+        </li>
+        <li>
+            <a href="#">About</a>
+        </li>
+        <li>
+            <a href="#">Portfolio</a>
+        </li>
+        <li>
+            <a href="#">Contact</a>
+        </li>
+    </ul>
+</nav>
