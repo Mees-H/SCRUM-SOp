fdf6c502f533bd5b5e385c8a366025490d93f735
ChendoMonster404 <chendo.monster@gmail.com>
2023-05-02 14:38:30 +0200

    Toegankelijkheid links

--- resources/views/sitemap/links.blade.php
+++ resources/views/sitemap/links.blade.php
@@ -13,7 +13,7 @@
                         <div class="col">
                             @foreach($category['links'] as $link)
                                 <div class="row">
-                                    <a title="{{$link['alt']}}" href="{{$link['link']}}">{{$link['name']}}</a>
+                                    <a href="{{$link['link']}}" alt="{{$link['alt']}}">{{$link['name']}}</a>
                                 </div>
                             @endforeach
                         </div>
