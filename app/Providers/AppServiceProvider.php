279127b3f4b3e1b17fb12581e34d764023390b2e
koen verstappen <koenverstappen2003@gmail.com>
2023-05-10 14:10:00 +0200

    yeetus deletus

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -20,6 +20,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        if(env('APP_ENV') !== 'local'){
+            \URL::forceScheme('https');
+        }
+
         Schema::defaultStringLength(191);
 
         if (Schema::hasTable('albums')) {
