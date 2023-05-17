ba291a82662d9274fd37d1cc24e05d33926922ff
ChendoMonster404 <chendo.monster@gmail.com>
2023-05-17 19:50:36 +0200

    responsive table, footer not yet fixed

--- resources/views/gallery/index.blade.php
+++ resources/views/gallery/index.blade.php
@@ -9,33 +9,35 @@
             {{ session()->get('success') }}
         </div>
     @endif
-    <table class="table table-striped">
-        <thead>
-            <tr>
-                <td>ID</td>
-                <td>Titel</td>
-                <td>Beschrijving</td>
-            </tr>
-        </thead>
-        <tbody>
-            @foreach($albums as $album)
+    <div class="table-responsive">
+        <table class="table table-striped">
+            <thead>
                 <tr>
-                    <td>{{$album->id}}</td>
-                    <td>{{$album->title}} </td>
-                    <td>{{$album->description}}</td>
-                    <td>{{ \Carbon\Carbon::parse($album->date)->format('d-m-Y')}}</td>
-                    <td>
-                        <a href="{{ route('galerij.edit',$album->id)}}" class="btn btn-primary" dusk="editAlbum">Aanpassen</a>
-                    </td>
-                    <td>
-                        <form action="{{ route('galerij.destroy', $album->id)}}" method="post">
-                        @csrf
-                        @method('DELETE')
-                        <button class="btn btn-danger" type="submit" dusk="deleteAlbum">Verwijderen</button>
-                        </form>
-                    </td>
+                    <td>ID</td>
+                    <td>Titel</td>
+                    <td>Beschrijving</td>
                 </tr>
-            @endforeach
-        </tbody>
-    </table>
+            </thead>
+            <tbody>
+                @foreach($albums as $album)
+                    <tr>
+                        <td>{{$album->id}}</td>
+                        <td>{{$album->title}} </td>
+                        <td>{{$album->description}}</td>
+                        <td>{{ \Carbon\Carbon::parse($album->date)->format('d-m-Y')}}</td>
+                        <td>
+                            <a href="{{ route('galerij.edit',$album->id)}}" class="btn btn-primary" dusk="editAlbum">Aanpassen</a>
+                        </td>
+                        <td>
+                            <form action="{{ route('galerij.destroy', $album->id)}}" method="post">
+                            @csrf
+                            @method('DELETE')
+                            <button class="btn btn-danger" type="submit" dusk="deleteAlbum">Verwijderen</button>
+                            </form>
+                        </td>
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
+    <div>
 @endsection
