00ff787c5db18ae4c171962fcb0bc5389d591df0
Phwdm <ph.wildeman@student.avans.nl>
2023-05-18 12:22:22 +0200

    appserviceprovider fix

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -20,18 +20,21 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        if(env('APP_ENV') === 'local') {
+            \URL::forceScheme('http');
+        }
+        else if(env('APP_ENV') === 'production') {
+            \URL::forceScheme('http');
+        }
+        else {
+            error_log('Check of de APP_ENV op local of production staat. Run dan het volgende commando: "php artisan optimize:clear" -Mees');
+        }
+
         Schema::defaultStringLength(191);
 
         if (Schema::hasTable('albums')) {
             $allYears = (new \App\Http\Controllers\GalleryController())->ShowAllYearsOfGallerys();
             view()->share('allYears', $allYears);
-        }
-        Schema::defaultStringLength(191);
-
-        if (Schema::hasTable('news_articles')) {
-
-            $yearsOfNewsArticles = (new \App\Http\Controllers\NewsArticleController())->ShowAllYearsOfNewsArticles();
-            view()->share('yearsOfNewsArticles', $yearsOfNewsArticles);
         }
     }
 }
