705df981469ff69174d8bf4a1d7a0b48440e6e66
Eva van Gurp <e.vangurp1@student.avans.nl>
2023-05-18 09:16:38 +0200

    Veelgestelde vragen admin responsive maken

--- resources/views/faq/index.blade.php
+++ resources/views/faq/index.blade.php
@@ -11,33 +11,35 @@
             {{ session()->get('success') }}
         </div>
     @endif
-    <table class="table table-striped">
-        <thead>
-            <tr>
-                <td>ID</td>
-                <td>Vraag</td>
-                <td>Antwoord</td>
-            </tr>
-        </thead>
-        <tbody>
-            @foreach($FAQ as $faq)
+    <div style="overflow-x:auto;">
+        <table class="table table-striped">
+            <thead>
                 <tr>
-                    <td>{{$faq->id}}</td>
-                    <td>{{$faq->question}} </td>
-                    <td>{{$faq->answer}}</td>
-                    <td>
-                        <a href="{{ route('faq.edit',$faq->id)}}" class="btn btn-primary">Aanpassen</a>
-                    </td>
-                    <td>
-                        <form action="{{ route('faq.destroy', $faq->id)}}" method="post">
-                        @csrf
-                        @method('DELETE')
-                        <button class="btn btn-danger" type="submit">Verwijderen</button>
-                        </form>
-                    </td>
+                    <td>ID</td>
+                    <td>Vraag</td>
+                    <td>Antwoord</td>
                 </tr>
-            @endforeach
-        </tbody>
-    </table>
+            </thead>
+            <tbody>
+                @foreach($FAQ as $faq)
+                    <tr>
+                        <td>{{$faq->id}}</td>
+                        <td>{{$faq->question}} </td>
+                        <td>{{$faq->answer}}</td>
+                        <td>
+                            <a href="{{ route('faq.edit',$faq->id)}}" class="btn btn-primary">Aanpassen</a>
+                        </td>
+                        <td>
+                            <form action="{{ route('faq.destroy', $faq->id)}}" method="post">
+                            @csrf
+                            @method('DELETE')
+                            <button class="btn btn-danger" type="submit">Verwijderen</button>
+                            </form>
+                        </td>
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
+    </div>
 </div>
 @stop
