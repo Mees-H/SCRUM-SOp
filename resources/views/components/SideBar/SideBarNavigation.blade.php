ad09e4db7d70b02f89d4621fa0c3e0f422638f32
Phwdm <ph.wildeman@student.avans.nl>
2023-05-26 12:11:48 +0200

    sidebar fix

--- resources/views/components/SideBar/SideBarNavigation.blade.php
+++ resources/views/components/SideBar/SideBarNavigation.blade.php
@@ -1,44 +1,47 @@
-<div class="px-3 mb-3 pt-3 bg-white card">
-<nav id="sidebar">
-    <div class="sidebar-header border-bottom">
-        <h3 id="sidebarHeader">Artikelen</h3>
-    </div>
+<head>
+    <link rel="stylesheet" href="{{asset('css/News.css')}}"/>
+    <title>NavigationSideBar</title>
+</head>
 
-    <ul class="list-unstyled components mb-0">
+<div class="wrapper p-3 bg-white card">
+    <nav id="sidebar">
+        <div class="sidebar-header border-bottom">
+            <h3 id="sidebarHeader">Artikelen Datums</h3>
+        </div>
 
-        <li class="active border-bottom">
-            <h3 id="sidebarHeader" class="pt-2">
-                Datums
-            </h3>
-            @foreach($yearsOfNewsArticles as $year)
-            <div class="d-flex justify-content-between">
 
-            <button class="btn btn-toggle d-inline-flex align-items-center border-0" data-bs-toggle="collapse" data-bs-target="#articleOfYear{{$year}}" aria-expanded="true">
-                {{$year}}
-                <i class="bi bi-arrow-down text-secondary"></i>
-            </button>
+        <ul class="list-unstyled components">
 
-            </div>
+            <li class="active border-bottom">
+                @if($years)
+                    @foreach($years as $year => $articles_year)
+                        <div class="d-flex justify-content-between">
+                            <button class="btn btn-toggle d-inline-flex align-items-center border-0" data-bs-toggle="collapse" data-bs-target="#articleOfYear{{$year}}" aria-expanded="true">
+                                {{$year}}
+                                <i class="bi bi-arrow-down text-secondary"></i>
+                            </button>
+                        </div>
 
-            <div class="collapse collapsed" id="articleOfYear{{$year}}">
-            <ul class="btn-toggle-nav list-unstyled fw-normal small">
+                        <div class="collapse collapsed" id="articleOfYear{{$year}}">
+                            <ul class="btn-toggle-nav list-unstyled fw-normal small">
 
-                    @foreach($articles as $article)
-                        <li>
-                            <a id="articleNav" href="#{{$article->id}}" class="border-0 text-decoration-none text-black ">
-                                <p id="navText" class="border-top pt-2">
-                                {{$article->title}}
-                                    <br/>
-                                    {{date('d-m', strtotime($article->date))}}
-                                </p>
-                            </a>
-                        </li>
+                                @foreach($articles_year as $article)
+                                    <li>
+                                        <a id="articleNav" href="#{{$article['id']}}" class="border-0 text-decoration-none text-black ">
+                                            <p id="navText" class="border-top pt-2">
+                                                {{$article['title']}}
+                                                <br/>
+                                                {{date('d-m', strtotime($article['date']))}}
+                                            </p>
+                                        </a>
+                                    </li>
+                                @endforeach
+
+                            </ul>
+                        </div>
                     @endforeach
-
-            </ul>
-            </div>
-            @endforeach
-        </li>
-    </ul>
-</nav>
+                @endif
+            </li>
+        </ul>
+    </nav>
 </div>
