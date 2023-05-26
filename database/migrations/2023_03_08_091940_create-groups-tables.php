ced98292894c7ae05e697bf906b03cec792beebc
Luuk <luukvink04@gmail.com>
2023-05-26 14:07:54 +0200

    chore(partners): changed group db object to also contain nessecary fields for partner exposure

--- database/migrations/2023_03_08_091940_create-groups-tables.php
+++ database/migrations/2023_03_08_091940_create-groups-tables.php
@@ -4,14 +4,13 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
     public function up(): void
     {
-        Schema::create('groups', function (Blueprint $table){
+        Schema::create('groups', function (Blueprint $table) {
             $table->id();
             $table->string('name')->unique();
             $table->integer('housenumber');
@@ -19,7 +18,9 @@ return new class extends Migration
             $table->string('zipcode');
             $table->string('city');
             $table->string('link');
+            $table->string('contact_person');
             $table->string('imageurl');
+            $table->boolean('currently_a_partner');
             $table->timestamps();
         });
     }
