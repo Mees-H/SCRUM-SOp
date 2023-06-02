2ec6a7597e01fcd2676a46ac880d05328d7ba3f5
Mees-H <meesconfirmed@gmail.com>
2023-06-02 08:34:34 +0200

    fixed date format

--- resources/views/gallery/index.blade.php
+++ resources/views/gallery/index.blade.php
@@ -25,7 +25,7 @@
                         <td>{{$album->id}}</td>
                         <td>{{$album->title}} </td>
                         <td>{{$album->description}}</td>
-                        <td>{{$album->date}}</td>
+                        <td>{{ \Carbon\Carbon::parse($album->date)->format('d-m-Y')}}</td>
                         <td>
                             <a dusk="editAlbum" href="{{ route('galerij.edit',$album->id)}}" class="btn btn-primary">Aanpassen</a>
                         </td>
