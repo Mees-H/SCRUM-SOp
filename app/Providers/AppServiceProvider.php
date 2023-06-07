4d726bddd9e9edc8c8b389a6eb5e999a827b4c25
Mees-H <meesconfirmed@gmail.com>
2023-06-07 09:55:18 +0200

    prep dynamic banner

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\Blade;
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
 
@@ -33,5 +34,13 @@ class AppServiceProvider extends ServiceProvider
             $allYears = (new \App\Http\Controllers\GalleryController())->ShowAllYearsOfGallerys();
             view()->share('allYears', $allYears);
         }
+
+        Blade::directive('banner', function ($expression) {
+            // Get the current URL
+            $url = request()->url();
+
+            // Return the path to the banner picture based on the URL
+            return "<?php echo asset('img/banners/' . {$url} . '.*'); ?>";
+        });
     }
 }
