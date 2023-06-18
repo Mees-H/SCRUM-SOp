d99a5e1c6cac4f8fa4b104ccb4fd6eaca68df357
Qoen1 <k.verstappen1@student.avans.nl>
2023-06-19 00:41:19 +0200

    paar kolommen verwijderd en een kleur toegevoegd die aangeeft of iets een vakantie is

--- resources/views/training/index.blade.php
+++ resources/views/training/index.blade.php
@@ -6,6 +6,10 @@
         <h1 class="display-3">Trainingen</h1>
         <div class="justify-content-between d-lg-flex mb-3">
             <a id="Creeër nieuwe training" href="{{ route('trainingsessions.create')}}" class="btn btn-primary">Creeër nieuwe training</a>
+            <div class="d-flex align-items-center">
+                <div class="bg-info" style="min-width: 1em;min-height: 1em;max-width: 1em;max-height: 1em"></div>
+                <span> = vakantie week</span>
+            </div>
         </div>
         @if(session()->get('success'))
             <div class="alert alert-success">
@@ -36,7 +40,7 @@
                             <td>Groep {{$session->GroupNumber}}</td>
                             <td>
                                 <a href="{{ route('trainingsessions.edit',$session->Id)}}" class="btn btn-primary">Aanpassen</a>
-                            
+
                                 <form action="{{ route('trainingsessions.destroy', $session->Id)}}" method="post">
                                 @csrf
                                 @method('DELETE')
@@ -56,26 +60,20 @@
             <table class="table table-striped">
                 <thead>
                     <tr>
-                        <td>ID</td>
                         <td>Datum</td>
-                        <td>Begintijd</td>
-                        <td>Eindtijd</td>
+                        <td>tijd</td>
                         <td>Beschrijving</td>
                         <td>Groep</td>
-                        <td>Vakantieweek</td>
                         <td colspan = 2>Actions</td>
                     </tr>
                 </thead>
                 <tbody>
                     @foreach($sessions as $session)
-                        <tr>
-                            <td>{{$session->Id}}</td>
+                        <tr @if(!$session->IstrainingSession)class="bg-info border-info" @endif>
                             <td>{{ \Carbon\Carbon::parse($session->Date)->format('d-m-Y')}}</td>
-                            <td>{{date('H:i', strtotime($session->StartTime))}}</td>
-                            <td>{{date('H:i', strtotime($session->EndTime))}}</td>
+                            <td>{{date('H:i', strtotime($session->StartTime))}} - {{date('H:i', strtotime($session->EndTime))}}</td>
                             <td>{{$session->Description}}</td>
                             <td>Groep {{$session->GroupNumber}}</td>
-                            <td>{{$session->IstrainingSession == 1 ? 'X' : '✓'}}</td>
                             <td>
                                 <a href="{{ route('trainingsessions.edit',$session->Id)}}" class="btn btn-primary">Aanpassen</a>
                             </td>
@@ -92,7 +90,7 @@
             </table>
         </div>
         @endif
-        
+
     <div>
 </div>
 @endsection
