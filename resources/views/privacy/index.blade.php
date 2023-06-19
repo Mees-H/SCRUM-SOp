5da9e9589e2f33b4ef2e8cf5aeecea1c88a20b32
Mees-H <meesconfirmed@gmail.com>
2023-06-19 10:32:56 +0200

    fixed privacy

--- resources/views/privacy/index.blade.php
+++ resources/views/privacy/index.blade.php
@@ -1,6 +1,17 @@
 @extends('layouts.layout')
 
 @section('content')
+    <div class="card mb-3">
+        <div class="card-body">
+        Wij hechten er veel waarde aan om uw privacy te waarborgen en gaan daarom zorgvuldig om met persoonsgegevens. 
+        Wij houden ons in alle gevallen aan de toepasselijke wet- en regelgeving. 
+        Dit brengt met zich mee dat wij: uw persoonsgegevens verwerken in overeenstemming met het doel waarvoor deze zijn verstrekt, 
+        de verwerking van persoonsgegevens beperken tot gegevens welke minimaal nodig zijn voor de doeleinden waarvoor ze worden verwerkt, 
+        passende organisatorische maatregelen nemen, zodat de beveiliging van uw persoonsgegevens gewaarborgd zijn, 
+        geen persoonsgegevens worden doorgeven aan andere partijen, 
+        tenzij dit nodig is voor uitvoering van de doeleinden waarvoor ze worden verstrekt.
+        </div>
+    </div>
     @if ($errors->any())
         <div class="alert alert-danger mt-5">
             <ul class="list-unstyled">
