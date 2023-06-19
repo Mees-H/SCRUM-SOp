4a90cb2b7caa0d9aac1eeda7258e18c986c750b1
JoshavEngelen <josha@mixnmac.nl>
2023-06-19 12:31:41 +0200

    Fixed test

--- tests/Feature/BannerTest.php
+++ tests/Feature/BannerTest.php
@@ -3,9 +3,11 @@
 namespace Tests\Feature;
 
 use App\Models\User;
+use App\Models\Page;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Http\UploadedFile;
+use Illuminate\Support\Facades\File;
 use Tests\TestCase;
 
 class BannerTest extends TestCase
@@ -19,6 +21,12 @@ class BannerTest extends TestCase
             'role' => 'admin'
         ]);
         
+        $page = Page::first();
+
+        $originalBanner = $page->banner_image;
+
+        File::copy(public_path('img/banners/' . $page->banner_image), public_path('img/banners/tmpimg.jpg'));
+
         $file = UploadedFile::fake()->create('image.jpg', 200);
 
         $request = [
@@ -28,7 +36,7 @@ class BannerTest extends TestCase
         try {
             //Act
             $this->actingAs($user);
-            $response = $this->post('banners/1', $request);
+            $response = $this->post('banners/' . $page->id, $request);
 
         } catch (\Exception $e) {
             echo "<script>console.log(" . $e . ");</script>";
@@ -36,5 +44,13 @@ class BannerTest extends TestCase
 
         //Assert
         $response->assertRedirect('/banners');
+
+        //Cleanup
+        $page = Page::find($page->id);
+        File::delete(public_path('img/banners/' . $page->banner_image));
+        File::move(public_path('img/banners/tmpimg.jpg'), public_path('img/banners/' . $originalBanner));
+        $page->banner_image = $originalBanner;
+        $page->save();
+
     }
 }
