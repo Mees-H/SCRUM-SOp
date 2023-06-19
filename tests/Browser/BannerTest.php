402216255b6c244f5edccb2d941a011709ac9c1a
JoshavEngelen <josha@mixnmac.nl>
2023-06-19 10:50:45 +0200

    Removed useless test

--- tests/Browser/BannerTest.php
+++ tests/Browser/BannerTest.php
@@ -31,20 +31,4 @@ class BannerTest extends DuskTestCase
                     ->assertSee('Je moet een afbeelding selecteren');
         });
     }
-
-    //Test werkt niet als de naam van de banner niet overeenkomt met de naam van de banner in de seeddata
-    public function testUpdateBanner(): void
-    {
-        $this->browse(function (Browser $browser) {
-            $page = Page::first();
-
-            $browser->loginAs(User::find(1))
-                    ->visit('/banners')
-                    ->resize(3000,3000)
-                    ->press('@' . $page->title . '_banner_aanpassen')
-                    ->attach('image', ('public/img/banners/' . $page->banner_image))
-                    ->press('@upload')
-                    ->assertSee('Banner is aangepast');
-        });
-    }
 }
