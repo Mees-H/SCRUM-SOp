453c9a7c1b44d57dde4abcb98c342d9be8786521
Eva van Gurp <imcheeze115@gmail.com>
2023-06-19 12:51:21 +0200

    Update training.blade.php

--- resources/views/training/training.blade.php
+++ resources/views/training/training.blade.php
@@ -6,14 +6,14 @@
     <form class="row justify-content-center d-flex" action="/training" method="post">
         @csrf
         <input type="hidden" name="year" value="{{$year}}"/>
-        <button class="btn btn-primary col-md-1" type="submit" name="weekNumber" value="{{$weekFrom - 4}}" aria-label="Knop om vorige 4 weken te bekijken">
+        <button class="btn btn-primary col-auto" type="submit" name="weekNumber" value="{{$weekFrom - 4}}" aria-label="Knop om vorige 4 weken te bekijken">
             <i class="fa-solid fa-caret-left"></i>
         </button>
-        <div class="col-3">
+        <div class="col-auto">
             <h3 class="text-center">{{$year}}</h3>
             <h3 class="text-center">Week {{$weekFrom}} - {{$weekTo}}</h3>
         </div>
-        <button class="btn btn-primary col-md-1" type="submit" name="weekNumber" value="{{$weekFrom + 4}}" aria-label="Knop om volgende 4 weken te bekijken">
+        <button class="btn btn-primary col-auto" type="submit" name="weekNumber" value="{{$weekFrom + 4}}" aria-label="Knop om volgende 4 weken te bekijken">
             <i class="fa-solid fa-caret-right"></i>
         </button>
     </form>
@@ -27,13 +27,14 @@
                     <div class="row justify-content-center">
                     @if($session->weekNumber == $i && $session->year == $year)
                         @if(!$session->IstrainingSession)
-                            <div class="col-auto">{{$session->Description}}</div>
                             <div class="col-auto">{{$session->weekDay}} {{$session->day}} {{$session->month}}</div>
+                            <div class="col-auto">{{$session->Description}}</div>
                             @php($sessionAmount++)
                         @else
                             <div class="col-auto">{{$session->weekDay}} {{$session->day}} {{$session->month}}</div>
-                            <div class="col-auto">{{$session->StartTime}}-{{$session->EndTime}}</div>
+                            <div class="col-auto">{{date('H:i', strtotime($session->StartTime))}}-{{date('H:i', strtotime($session->EndTime))}}</div>
                             <div class="col-auto">{{$session->training_session_group->Name}}</div>
+                            <div class="col-auto">{{$session->Description}}</div>
                             @php($sessionAmount++)
                         @endif
                     @endif
