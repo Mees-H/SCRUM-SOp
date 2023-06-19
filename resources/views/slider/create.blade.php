9969296df939b49ba06daafdeaf71be4b18da4d8
koen verstappen <koenverstappen2003@gmail.com>
2023-06-19 15:43:33 +0200

    warning toegevoegd

--- resources/views/slider/create.blade.php
+++ resources/views/slider/create.blade.php
@@ -13,18 +13,7 @@
                         </div>
                     @endif
                 </div>
-                <form class="col-md-6 image-uplode d-inline-block border shadow-lg rounded p-2 mt-5"
-                      action="{{ route('slider.store') }}" method="POST" enctype="multipart/form-data">
-                    @csrf
-                    <div class="m-5">
-                        <h1 class=" mb-5 display-3">Upload afbeelding voor slider</h1>
-                        <div class="w-100 alert alert-warning col-md-6 text-center"><p>de afbeelding mag niet groter dan 2 GB zijn.</p></div>
-                        <input type="file" class="form-control form-control-lg" name="image" id="image">
-                    </div>
-                    <div class="m-5">
-                        <button class="btn btn-primary">Upload Afbeelding</button>
-                    </div>
-                </form>
+                <x-image-upload-form action="{{ route('slider.store') }}" label="'Upload afbeelding voor slider'" />
             </div>
         </div>
     </div>
