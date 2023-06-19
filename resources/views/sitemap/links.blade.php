6794b4340defb1a38b06e06773f5590fbb1e2c4e
Phwdm <ph.wildeman@student.avans.nl>
2023-06-19 11:37:50 +0200

    Fixed it for admin page

--- resources/views/sitemap/links.blade.php
+++ resources/views/sitemap/links.blade.php
@@ -1,14 +1,14 @@
 @extends('layouts.layout')
 
 @section('content')
-    <div class="container">
+    <div class="px-4">
         <h1 class="text-center pt-2 display-3">Links</h1>
-        <div class="d-flex justify-content-center">
+        <div class="d-flex row justify-content-center">
             @foreach($links as $category)
-                <div class="row-cols-md m-3 justify-content-around">
+                <div class="col-md-auto  p-3">
                     <h2>{{$category['name']}}</h2>
                     @foreach($category['links'] as $link)
-                        <div class="row">
+                        <div>
                             <a href="{{$link['link']}}" alt="{{$link['alt']}}">{{$link['name']}}</a>
                         </div>
                     @endforeach
