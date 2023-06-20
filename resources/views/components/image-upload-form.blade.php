aa3dabdb99e481d0a8d80853d62d6fba7ef47c91
Qoen1 <k.verstappen1@student.avans.nl>
2023-06-20 15:42:43 +0200

    Update image-upload-form.blade.php

--- resources/views/components/image-upload-form.blade.php
+++ resources/views/components/image-upload-form.blade.php
@@ -2,7 +2,7 @@
     @csrf
     <div class="m-5">
         <h1 class=" mb-5 display-3">{{ $label }}</h1>
-        <div class="w-100 alert alert-warning col-md-6 text-center"><p>de afbeelding mag niet groter dan 2 GB zijn.</p></div>
+        <div class="w-100 alert alert-warning col-md-6 text-center"><p>de afbeelding mag niet groter dan 2 MB zijn.</p></div>
         <input type="file" class="form-control" name="image" id="image">
     </div>
     <div class="m-5">
